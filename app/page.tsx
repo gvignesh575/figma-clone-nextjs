caf852ad18bd39ebf62f8b030397a3e2e8c6b6ee
GVignesh-SD <gvignesh380980@gmail.com>
2024-02-19 18:24:04 +0530

    implemented the live cursor and live chat functionality!

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Live from "@/components/Live";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Live />
+    </div>
+  );
+};
+
+export default page;
